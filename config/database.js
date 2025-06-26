e646a390ebf939b4e376996dd391eaa54754d1d3
Levi <duartelevi1@gmail.com>
2025-06-26 19:10:52 -0300

    .

--- config/database.js
+++ config/database.js
@@ -1,5 +1,9 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
+console.log('MYSQL_DATABASE:', process.env.MYSQLDATABASE);
+console.log('MYSQL_USER:', process.env.MYSQLUSER);
+console.log('MYSQL_PASSWORD:', process.env.MYSQLPASSWORD ? '***' : 'NÃO DEFINIDA');
+console.log('MYSQL_HOST:', process.env.MYSQLHOST);
 
 const sequelize = new Sequelize(
   process.env.MYSQLDATABASE,
@@ -11,9 +15,6 @@ const sequelize = new Sequelize(
     logging: false,
   }
 );
-console.log('MYSQL_DATABASE:', process.env.MYSQLDATABASE);
-console.log('MYSQL_USER:', process.env.MYSQLUSER);
-console.log('MYSQL_PASSWORD:', process.env.MYSQLPASSWORD ? '***' : 'NÃO DEFINIDA');
-console.log('MYSQL_HOST:', process.env.MYSQLHOST);
+
 
 module.exports = sequelize;
