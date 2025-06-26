76a3f57a59fd94d3be1944080943b8abbf334352
Levi <duartelevi1@gmail.com>
2025-06-26 19:02:20 -0300

    .

--- config/database.js
+++ config/database.js
@@ -11,5 +11,9 @@ const sequelize = new Sequelize(
     logging: false,
   }
 );
+console.log('MYSQL_DATABASE:', process.env.MYSQL_DATABASE);
+console.log('MYSQL_USER:', process.env.MYSQL_USER);
+console.log('MYSQL_PASSWORD:', process.env.MYSQL_PASSWORD ? '***' : 'NÃO DEFINIDA');
+console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
 
-module.exports = sequelize; 
+module.exports = sequelize;
