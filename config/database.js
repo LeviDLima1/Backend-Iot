ad34f38b1e9f75fcbbc17383dd178396ff134084
Levi <duartelevi1@gmail.com>
2025-06-26 21:43:09 -0300

    .

--- config/database.js
+++ config/database.js
@@ -1,26 +1,28 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
-console.log('MYSQL_DATABASE:', process.env.MYSQLDATABASE);
-console.log('MYSQL_USER:', process.env.MYSQLUSER);
-console.log('MYSQL_PASSWORD:', process.env.MYSQLPASSWORD ? '***' : 'NÃO DEFINIDA');
-console.log('MYSQL_HOST:', process.env.MYSQLHOST);
 
-const MYSQL_DATABASE = process.env.MYSQLDATABASE
-const MYSQL_USER = process.env.MYSQLUSER
-const MYSQL_PASSWORD = process.env.MYSQLPASSWORD
-const MYSQL_HOST = process.env.MYSQLHOST
+// Só carrega dotenv em desenvolvimento
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
+console.log('MYSQL_DATABASE:', process.env.MYSQL_DATABASE);
+console.log('MYSQL_USER:', process.env.MYSQL_USER);
+console.log('MYSQL_PASSWORD:', process.env.MYSQL_PASSWORD ? '***' : 'NÃO DEFINIDA');
+console.log('MYSQL_HOST:', process.env.MYSQL_HOST);
+console.log('MYSQL_PORT:', process.env.MYSQL_PORT);
 
 const sequelize = new Sequelize(
-  MYSQL_DATABASE,
-  MYSQL_USER,
-  MYSQL_PASSWORD,
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
   {
-    host: MYSQL_HOST,
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT || 3306,
     dialect: 'mysql',
     logging: false,
   }
 );
+
 
 const testConnection = async () => {
   try {
