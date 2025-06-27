37e230a82911c9fa1ab0d953bbb74be4dc86d4e3
Levi <duartelevi1@gmail.com>
2025-06-26 23:15:16 -0300

    .

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+// CORS ajustado para aceitar todos os métodos e headers necessários
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 require('./config/sync');
 
-app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
