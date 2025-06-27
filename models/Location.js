b355959d549cb6809479382a056b274f8bd92359
Levi <duartelevi1@gmail.com>
2025-06-26 22:03:36 -0300

    .

--- models/Location.js
+++ models/Location.js
@@ -1,41 +1,41 @@
-module.exports = (sequelize, DataTypes) => {
-  const Location = sequelize.define('Location', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    petId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'pets',
-        key: 'id',
-      },
-      onDelete: 'CASCADE',
-    },
-    latitude: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    longitude: {
-      type: DataTypes.DOUBLE,
-      allowNull: false,
-    },
-    timestamp: {
-      type: DataTypes.DATE,
-      allowNull: false,
-      defaultValue: DataTypes.NOW,
-    },
-  }, {
-    tableName: 'locations',
-    timestamps: false,
-  });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Pet = require('./Pet');
 
-  Location.associate = (models) => {
-    models.Pet.hasMany(Location, { foreignKey: 'petId', as: 'locations' });
-    Location.belongsTo(models.Pet, { foreignKey: 'petId', as: 'pet' });
-  };
+const Location = sequelize.define('Location', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  petId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'pets',
+      key: 'id',
+    },
+    onDelete: 'CASCADE',
+  },
+  latitude: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  longitude: {
+    type: DataTypes.DOUBLE,
+    allowNull: false,
+  },
+  timestamp: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+}, {
+  tableName: 'locations',
+  timestamps: false,
+});
 
-  return Location;
-}; 
+Pet.hasMany(Location, { foreignKey: 'petId', as: 'locations' });
+Location.belongsTo(Pet, { foreignKey: 'petId', as: 'pet' });
+
+module.exports = Location; 
--- models/Pet.js
+++ models/Pet.js
@@ -1,55 +1,64 @@
-module.exports = (sequelize, DataTypes) => {
-  const Pet = sequelize.define('Pet', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    breed: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    age: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    macId: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    owner: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    lastUpdate: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    isOnline: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-    homeArea: {
-      type: DataTypes.JSON,
-      allowNull: true,
-    },
-    location: {
-      type: DataTypes.JSON,
-      allowNull: true,
-    },
-  }, {
-    tableName: 'pets',
-  });
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./User');
 
-  Pet.associate = (models) => {
-    models.User.hasMany(Pet, { foreignKey: 'userId', as: 'pets' });
-    Pet.belongsTo(models.User, { foreignKey: 'userId', as: 'user' });
-  };
+const Pet = sequelize.define('Pet', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  breed: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  age: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  macId: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  owner: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  lastUpdate: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  isOnline: {
+    type: DataTypes.BOOLEAN,
+    defaultValue: false,
+  },
+  homeArea: {
+    type: DataTypes.JSON,
+    allowNull: true,
+  },
+  location: {
+    type: DataTypes.JSON,
+    allowNull: true,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id',
+    },
+    onDelete: 'CASCADE',
+  },
+}, {
+  tableName: 'pets',
+});
 
-  return Pet;
-}; 
+User.hasMany(Pet, { foreignKey: 'userId', as: 'pets' });
+Pet.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+module.exports = Pet; 
